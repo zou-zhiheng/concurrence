e6c49f1d2869e0014464aa31b8f72f6f585c3a81
sttch <421786612@qq.com>
2023-06-30 21:56:12 +0800

    workPool

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package  main
 
 import (
 	"concurrence/ProducerConsumer"
+	"concurrence/WorkPool"
 )
 
 func main(){
 	ProducerConsumer.ProducerConsumerDemo()
-	//WorkPool.Demo()
+	WorkPool.Demo()
 }
