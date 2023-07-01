9f5f781fb72d1dffde8cae428dcc03879eb22fe1
sttch <421786612@qq.com>
2023-07-01 21:08:51 +0800

    review

--- main.go
+++ main.go
@@ -3,11 +3,9 @@ package  main
 import (
 	"concurrence/ProducerConsumer"
 	"concurrence/WorkPool"
-	"fmt"
 )
 
 func main(){
-	fmt.Println("start")
 	ProducerConsumer.ProducerConsumerDemo()
 	WorkPool.Demo()
 }
